51e23c13732e4b97cd49ecb9ca24763bad7adf13
Damien Metzler <dmetzler@nuxeo.com>
2017-08-09 16:42:45 +0200

    fix

--- src/main/java/org/nuxeo/sample/NoteCreationListener.java
+++ src/main/java/org/nuxeo/sample/NoteCreationListener.java
@@ -22,6 +22,6 @@ public class NoteCreationListener implements EventListener {
         DocumentModel doc = docCtx.getSourceDocument();
 
         // Add some logic starting from here.
-        doc.setPropertyValue("dc:description","Happily deployed thru s2i.")
+        doc.setPropertyValue("dc:description","Happily deployed thru s2i.");
     }
 }
